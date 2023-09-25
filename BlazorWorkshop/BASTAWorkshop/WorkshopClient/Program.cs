d3d6bbf93871d8e8936c4d57a9cacb977ab11a9a
Patrick Jahr <patrick.jahr@googlemail.com>
2023-09-25 12:00:26 +0200

    add client project

---
+++ BlazorWorkshop/BASTAWorkshop/WorkshopClient/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using WorkshopClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7218/api/") });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
